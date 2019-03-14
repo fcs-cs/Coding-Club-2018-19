1eeb1dff1c8b5d8cc2befbdcdc710983491d3116
seentheshadow <srobertmcgoff@gmail.com>
2019-03-14 09:44:39 -0400

    comments and added pow

--- Sean/calc.js
+++ Sean/calc.js
@@ -1,12 +1,7 @@
-let app = new PIXI.Application({
-  antialias: true,
-  transparent: true,
-  forceCanvas: false
-});
-
+//assigning html elements to variables
 let i = document.getElementById('calcInput');
 let e = document.getElementById('equals');
-
+//the operator functions.
 function add (a,b) {
   return Number(a)+Number(b);
 }
@@ -19,6 +14,10 @@ function mult(a,b) {
 function divide(a,b) {
   return Number(a)/Number(b);
 }
+function pow(a,b) {
+  return Math.pow(a,b);
+}
+//Checks if its a numeral or operator with two functions.
 function isNumeral(char) {
   if (char == '0' || char == '1' || char == '2' || char == '3' || char == '4' || char == '5' || char == '6' || char == '7' || char == '8' || char == '9' || char == '.') {
     return true;
@@ -26,21 +25,22 @@ function isNumeral(char) {
   return false;
 }
 function isOperator(char) {
-  if (char == '+' || char == '-' || char == '*' || char == '/') {
+  if (char == '+' || char == '-' || char == '*' || char == '/' || char == '^') {
     return true;
   }
   return false;
 }
+//the function that runs when equals button is pressed
 function calculate() {
   //input box in html file
   let box = i.value;
-  //s1 is a temporary array for all number chars while s2 is for operands. Nulls are used as placeholders
+  //s1 is a temporary array for all number chars while s2 is for operands.
   let s1 = [];
   let s2 = [];
   //the array of numbers and operands that'll eventually be run through calc functions
   let nums = [];
   let ops = [];
-
+  //for loop that uses isNumeral and isOperator functions to sort every character in s1 in s2 arrays. Nulls are used as placeholders.
   for (let i = 0; i < box.length; i++) {
     if (isNumeral(box[i])) {
       s1[i] = box[i];
@@ -53,28 +53,24 @@ function calculate() {
     s2[i] = null;
   }
 }
-console.log(s1);
-console.log(s2);
+  //nb stores the string of number that'll go into the nums array
   let nb = '';
-  let nac = 0;
-  console.log(s2.length);
+  //sorts all the groups of numbers in arrays into numbers without nulls and all that
   for (let i = 0; i < s2.length; i++) {
     if (s1[i] !== null) {
       nb = nb + s1[i];
     } else {
-      nums[nac] = nb;
-      ops[nac] = s2[i];
-      nac++;
+      nums.push(nb);
+      ops.push(s2[i]);
       nb = '';
     }
   }
-  nums[nac] = nb;
-
-  console.log(nums);
-  console.log(ops);
+  nums.push(nb);
+  //set the value to the first number
   let val = nums[0];
+  //calls operators functions to use operators
   for (let i = 0; i < ops.length; i++) {
-    if (ops[i] == '+') {
+      if (ops[i] == '+') {
       val = add(val, nums[i+1]);
     } else if (ops[i] == '-') {
       val = subtract(val, nums[i+1]);
@@ -82,7 +78,10 @@ console.log(s2);
       val = mult(val, nums[i+1]);
     } else if (ops[i] == '/') {
       val = mult(val, nums[i+1]);
+    } else if (ops[i] == '^') {
+      val = pow(val, nums[i+1]);
     }
   }
+  //pushes val to the bottom
   document.getElementById('answer').innerHTML = 'The Answer is: ' + val;
 }
